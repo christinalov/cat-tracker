df0c8fde9580afdb00805626362f9adb9e772187
Christina Lov <christinalov@berkeley.edu>
2025-10-17 01:46:47 -0700

    figuring out persistent dates to local storage

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const currentDayObj = new Date();
 const currentMonth = currentDayObj.getMonth() + 1;
@@ -6,6 +6,14 @@ const currentDate = currentDayObj.getDate();
 const currentYear = currentDayObj.getFullYear();
 
 const currentDateMsg = `Today is ${currentMonth}/${currentDate}/${currentYear}`;
-const dateHeading = document.querySelector('#currentDate');
+const dateHeading = document.querySelector("#currentDate");
 dateHeading.textContent = currentDateMsg;
 
+const litterBoxDateObj = document.querySelector("#litterbox-date");
+
+const onLitterBoxDateChange = function () {
+  const selectedDate = litterBoxDateObj.value;
+  localStorage.setItem("litterBoxCleanDate", selectedDate);
+};
+
+litterBoxDateObj.addEventListener("change", onLitterBoxDateChange);
