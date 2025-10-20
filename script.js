d48c18f17e499fa04b7a03d07cf80b3241a40c52
Christina Lov <christinalov@berkeley.edu>
2025-10-20 01:24:42 -0700

    added karla font

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 "use strict";
 
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 const currentDayObj = new Date();
 const currentMonth = currentDayObj.getMonth() + 1;
 const currentDate = currentDayObj.getDate();
 const currentYear = currentDayObj.getFullYear();
+const currentDay = dayNames[currentDayObj.getDay()];
 
-const currentDateMsg = `Today is ${currentMonth}/${currentDate}/${currentYear}`;
+const currentDateMsg = `Today is ${currentMonth}/${currentDate}/${currentYear} (${currentDay})`;
 const dateHeading = document.querySelector("#currentDate");
 dateHeading.textContent = currentDateMsg;
 
-const litterBoxDateObj = document.querySelector("#litterbox-date");
+const litterBoxRoomDateObj = document.querySelector("#litterbox1-date");
+const litterBoxKitchenDateObj = document.querySelector("#litterbox2-date");
 
-const onLitterBoxDateChange = function () {
-  const selectedDate = litterBoxDateObj.value;
+const onLitterBoxDateChange = function (litterBoxRoom) {
+  const selectedDate = litterBoxRoom
+    ? litterBoxRoomDateObj.value
+    : litterBoxKitchenDateObj.value;
   localStorage.setItem("litterBoxCleanDate", selectedDate);
 };
 
-litterBoxDateObj.addEventListener("change", onLitterBoxDateChange);
+litterBoxRoomDateObj.addEventListener("change", onLitterBoxDateChange);
